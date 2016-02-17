24b6cb418d6afa873c14a79a906a1faf09417407
Srikanth Vishnuvajhala <srikanth.vishnuvajhala@gmail.com>
2016-02-16 23:25:01 -0800

    Initial Commit

---
+++ js/mainScript.js
@@ -0,0 +1,13 @@
+
+    $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+       
+    });
+        
+
+    $(".button-collapse").sideNav();
+        
+
+      $(document).ready(function(){
+      $('.parallax').parallax();
+    });
